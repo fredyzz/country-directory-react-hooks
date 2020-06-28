15318ecec1bee12f69de81681fd1c50d1965aaac
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2020-06-27 22:24:20 -0300

    Updates

---
+++ precache-manifest.1b294ef0f1cfc904032afad8a5958ad5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f4ce1a65ddb263646123e43c0bb86d7",
+    "url": "/country-directory-react-hooks/index.html"
+  },
+  {
+    "revision": "3b986d8fee6266500fb3",
+    "url": "/country-directory-react-hooks/static/css/main.edc8c69d.chunk.css"
+  },
+  {
+    "revision": "3aa36c72625c42708e0b",
+    "url": "/country-directory-react-hooks/static/js/2.37eefdf3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/country-directory-react-hooks/static/js/2.37eefdf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b986d8fee6266500fb3",
+    "url": "/country-directory-react-hooks/static/js/main.5f129390.chunk.js"
+  },
+  {
+    "revision": "eafed5453de68d5ac070",
+    "url": "/country-directory-react-hooks/static/js/runtime-main.d3fc9844.js"
+  }
+]);
